600250dc9f4ecf6f2c3dfc306410f5814f068fb6
munseongyun <borygashill608@gmail.com>
2024-08-01 18:44:34 +0900

    🛠️ Refactor : comment

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -21,9 +21,10 @@ class ImageController extends Controller
             $folder = $request->header('folder');
             // 파일 저장
             if(!Storage::disk('s3')->put("/{$folder}/{$fileName}", file_get_contents($request->file('image')))){
+                // 실패했을때 실패했다고 알려줌
                 return response()->json(['data'=>'fail'],400);
             };
-
+            // 성공하면 나중에 접근할 수 있도록, 파일 이름을 응답으로 줍니다.
             return response()->json(['data' => "/{$fileName}"], 201);
         } catch (\Exception $e) {
             return response()->json(['data' => 'error', 'message' => $e->getMessage()], 500);
@@ -38,7 +39,8 @@ class ImageController extends Controller
 
         $fileName = $request->query('fileName');
         $folder = $request->header('folder');
-        error_log($folder);
+        
+        // 10분동안 작동하는 임시 url을 만들어서 프론트에게 줍니다.
         $url = Storage::disk('s3')->temporaryUrl(
             "/{$folder}/{$fileName}", now()->addMinutes(10)
         );
@@ -55,6 +57,7 @@ class ImageController extends Controller
 
             $fileName = $request->query('fileName');
             $folder = $request->header('folder');
+            // 이미지 삭제
             Storage::disk('s3')->delete("{$folder}/{$fileName}");
 
             return response()->json(['data' => 'success'], 200);
