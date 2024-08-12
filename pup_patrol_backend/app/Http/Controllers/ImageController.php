df1548a1b30d5089ada95c5e6a24c1f030b1c030
munseongyun <borygashill608@gmail.com>
2024-08-12 23:33:45 +0900

    ⭐ Feature : breeze login

---
+++ pup_patrol_backend/app/Http/Controllers/ImageController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ImageController extends Controller
+{
+    //
+    public function upload(Request $request){
+        try {
+            // 파일 검증
+            $request->validate([
+                'image' => 'required|image|mimes:jpg,jpeg,png,gif|max:2048',
+            ]);
+
+            // 파일 이름 설정
+            $fileName = time() . '_' . $request->file('image')->getClientOriginalName();
+            // 파일 폴더 설정
+            $folder = $request->header('folder');
+            
+            // 파일 저장
+            if(!Storage::disk('s3')->put("/{$folder}/{$fileName}", file_get_contents($request->file('image')))){
+                // 실패했을때 실패했다고 알려줌
+                return response()->json(['data'=>'fail'],400);
+            };
+            // 성공하면 나중에 접근할 수 있도록, 파일 이름을 응답으로 줍니다.
+            return response()->json(['data' => "{$fileName}"], 201);
+        } catch (\Exception $e) {
+            return response()->json(['data' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function download(Request $request){
+        // 파일 이름 검증
+        $request->validate([
+            'fileName' => 'required|string',
+        ]);
+
+        $fileName = $request->query('fileName');
+        $folder = $request->header('folder');
+        
+        // 10분동안 작동하는 임시 url을 만들어서 프론트에게 줍니다.
+        $url = Storage::disk('s3')->temporaryUrl(
+            "/{$folder}/{$fileName}", now()->addMinutes(10)
+        );
+
+        return response()->json(['data' => $url], 200);
+    }
+
+    public function destroy(Request $request){
+        try {
+            // 파일 이름 검증
+            $request->validate([
+                'fileName' => 'required|string',
+            ]);
+
+            $fileName = $request->query('fileName');
+            $folder = $request->header('folder');
+            // 이미지 삭제
+            Storage::disk('s3')->delete("{$folder}/{$fileName}");
+
+            return response()->json(['data' => 'success'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['data' => 'error', 'message' => $e->getMessage()], 500);
+        }
+    }
+}
---
+++ pup_patrol_backend/routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\AnswerController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\InformationController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Middleware\IsAdmin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::get('answers',[AnswerController::class, 'index']);
+Route::get('informations', [InformationController::class, 'index']);
+Route::get('questions', [QuestionController::class, 'index']);
+Route::get('answers/{answer}',[AnswerController::class, 'show']);
+Route::get('informations/{information}', [InformationController::class, 'show']);
+Route::get('/imageDownload',[ImageController::class,'download']);
+
+Route::middleware('auth:sanctum')->middleware([IsAdmin::class])->group(function () {
+    Route::post('answers',[AnswerController::class, 'store']);
+    Route::post('informations', [InformationController::class, 'store']);
+    
+    Route::put('answers/{answer}',[AnswerController::class,'update']);
+    Route::put('informations/{information}',[InformationController::class,'update']);
+    
+    Route::patch('answers/{answer}',[AnswerController::class,'update']);
+    Route::patch('informations/{information}',[InformationController::class,'update']);
+    
+    Route::delete('answers/{answer}',[AnswerController::class, 'destroy']);
+    Route::delete('informations/{information}', [InformationController::class, 'destroy']);
+    
+    Route::post('/imageUpload',[ImageController::class,'upload']);
+    Route::delete('/imageDelete',[ImageController::class,'destroy']);
+});
+
+
+
